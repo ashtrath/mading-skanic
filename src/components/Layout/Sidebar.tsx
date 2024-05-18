a81e86afea46930eff9cf4f01c854494e0551279
Dyzean <ashtrath@pm.me>
2024-05-18 18:25:47 +0700

    Revert "feat: sidang wanjeennngggggggg"
    This reverts commit 10ed48e98adcbc1c5686a6045cafde8e57aa0dc8.

--- src/components/Layout/Sidebar.tsx
+++ src/components/Layout/Sidebar.tsx
@@ -1,9 +1,7 @@
 import { type Roles } from "@prisma/client";
-import { MaterialSymbol, type SymbolCodepoints } from "react-material-symbols";
+import { type SymbolCodepoints } from "react-material-symbols";
 import { useUser } from "~/context/userContext";
-import ProfileImage from "../ui/ProfileImage";
 import NavLink from "./NavLink";
-import { truncateText } from "~/utils";
 
 type SidebarProps = {
   items: {
@@ -12,17 +10,14 @@ type SidebarProps = {
     icon: SymbolCodepoints;
     roles?: Roles[];
   }[];
-  className?: string;
 };
 
-const Sidebar = ({ items, className }: SidebarProps) => {
+const Sidebar = ({ items }: SidebarProps) => {
   const { user } = useUser();
 
   return (
-    <aside
-      className={`flex min-h-full max-w-[220px] shrink-0 flex-col justify-between ${className}`}
-    >
-      <ul className="flex h-fit w-full flex-col gap-4 self-start border border-mono-black bg-mono-white px-6 py-4">
+    <aside className="sticky left-0 top-0 flex min-h-full min-w-[200px] max-w-[230px] shrink-0 flex-col justify-between">
+      <ul className="flex h-fit w-full flex-col gap-4 self-start border border-mono-black bg-mono-white px-8 py-4">
         {items.map((item, index) => {
           if (!item.roles || (user && item.roles.includes(user.role))) {
             return (
@@ -39,22 +34,9 @@ const Sidebar = ({ items, className }: SidebarProps) => {
           return null;
         })}
       </ul>
-      <div className="flex w-full items-center gap-2 border border-mono-black bg-mono-white px-4 py-2">
-        {/* <ProfileImage size="size-8" placeholderSize={32} /> */}
-        <div>
-          <h2 className="font-mono text-sm font-bold">{user?.name}</h2>
-          <p className="text-xs">
-            {truncateText("adminganteng@gmail.com", 15)}
-          </p>
-        </div>
-        <MaterialSymbol
-          icon="arrow_drop_up"
-          fill={false}
-          weight={200}
-          grade={0}
-          size={24}
-        />
-      </div>
+      {/* <div className="w-full border border-mono-black bg-mono-white px-8 py-4">
+        <ProfileImage size="32" placeholderSize={32} />
+      </div> */}
     </aside>
   );
 };
--- src/utils/validation/category.ts
+++ src/utils/validation/category.ts
@@ -1,7 +1,6 @@
 import z from "zod";
 
 export const categorySchema = z.object({
-  name: z.string().max(30, "Nama kategori tidak dapat lebih dari 30 karakter"),
+  id: z.string(),
+  // name: z.string().max(30, "Nama kategori tidak dapat lebih dari 30 karakter"),
 });
-
-export type ICategory = z.infer<typeof categorySchema>;
